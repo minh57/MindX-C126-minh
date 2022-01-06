6c6b9af0a3597c74689ff708d34c5ce60fa69dee
minh57 <viminh999999@gmail.com>
2022-01-06 09:57:48 +0700

    First commit

---
+++ C126/section 7/BTVN/minh.js
@@ -0,0 +1,17 @@
+// bài tập 1
+let x=parseFloat(prompt("Hãy nhập số bạn muốn căn bậc 2"))
+alert(Math.sqrt(a))
+// bài tập 2
+let y=parseFloat(prompt("Hãy nhập một cạnh của tam giác"))
+let h=parseFloat(prompt("Hãy nhập chiều cao của cạnh tương ứng đó"))
+alert(b*h/2)
+// bài tập 3
+let f=parseFloat(prompt("Hãy nhập nhiệt độ(độ C) bạn cần đổi"))
+alert(f+271,15)
+// bài tập 4
+let km=parseFloat(prompt("Hãy nhập độ dài bạn muốn quy đổi"))
+alert(km*1000)
+// bài tập 5
+let a="Hello"
+let b="MindX"
+console.log(a.)
