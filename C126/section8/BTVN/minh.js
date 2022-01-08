7314734d4a73c5cfcb8ff5ef425f214481e12e5e
minh57 <viminh999999@gmail.com>
2022-01-08 11:48:07 +0700

    hello

---
+++ C126/section8/BTVN/minh.js
@@ -0,0 +1,34 @@
+// // bài tập 1
+// let x = parseFloat(prompt("Hãy nhập biến thứ nhất"))
+// let y = parseFloat(prompt("Hãy nhập biến thứ hai"))
+// let result;
+// let dau = prompt("Hãy nhập phép tính")
+// if( dau == "+" ){
+// alert(`${x} + ${y} = ${x+y}`)
+// }else if ( dau == "-" ){
+//     alert( `${x} - ${y} = ${x-y}` ) 
+// }else if( dau == "*" ){
+//     alert( `${x} * ${y} = ${x * y}` )
+// }else {
+//     alert( `${x} / ${y} = ${x/y}` )
+// }
+
+
+// bài tập 2
+let n = parseFloat(prompt("Hãy nhập só"))
+if ( n == 1 || n == 0 ){
+    alert("Đây không phải là số nguyên tố")
+}else if(n==2){
+    alert("Đây là số nguyên tố")
+}else {
+    for(let i = 2;i<n;i++){
+        if( n%i == 0 )
+       { alert(`${n} không là số nguyên tố`)
+         break}
+         else{
+alert(`${n} là số nguyên tố`)
+break
+}
+
+}
+}
